53305164187d9d53ed4cce61deebfa1d8b5835a3
kyawakyawa <ikaairplanef15@yahoo.co.jp>
2019-08-19 18:10:39 +0900

    apply clang--format

--- tests/parts/number-functions01.cc
+++ tests/parts/number-functions01.cc
@@ -1,26 +1,30 @@
-#include <cstdlib>
 #include <cassert>
-#include <vector>
+#include <cstdlib>
 #include <tuple>
+#include <vector>
 
-template<typename T>
+template <typename T>
 constexpr std::tuple<T, T> ext_gcd(const T a, const T b) {
-  constexpr auto calc_xy = [](const T a, const T b,const std::tuple<T, T> xy) {
+  constexpr auto calc_xy = [](const T a, const T b, const std::tuple<T, T> xy) {
     const auto [x_dash, y_dash] = xy;
     return std::make_tuple(y_dash, x_dash - (a / b) * y_dash);
   };
 
-  return (b == static_cast<T>(0)) ? std::make_tuple(1, 0) : calc_xy(a, b, ext_gcd(b, a % b));
+  return (b == static_cast<T>(0)) ? std::make_tuple(1, 0)
+                                  : calc_xy(a, b, ext_gcd(b, a % b));
 }
 
-template<typename T>
+template <typename T>
 constexpr std::vector<std::tuple<T, T>> prime_factorization(T n) {
   std::vector<std::tuple<T, T>> ret;
 
   T p = 2;
   while (n >= p * p) {
     T index = static_cast<T>(0);
-    while (n % p == static_cast<T>(0)) {index++; n /= p;}
+    while (n % p == static_cast<T>(0)) {
+      index++;
+      n /= p;
+    }
     if (index > static_cast<T>(0)) ret.emplace_back(p, index);
     p++;
   }
@@ -60,7 +64,7 @@ constexpr T mod(const T a, const T m) {
 template <typename T>
 constexpr T euler_phi(const T n) {
   const std::vector<std::tuple<T, T>> factors = prime_factorization(n);
-  T ret = n;
+  T ret                                       = n;
   for (const auto [p, e] : factors) {
     ret *= (p - 1);
     ret /= p;
@@ -71,8 +75,8 @@ constexpr T euler_phi(const T n) {
 template <typename T>
 constexpr T find_one_of_primitive_root(const T p) {
   const std::vector<std::tuple<T, T>> factors = prime_factorization(p - 1);
-  const T phi = p - static_cast<T>(1);
-  T ret = static_cast<T>(2);
+  const T phi                                 = p - static_cast<T>(1);
+  T ret                                       = static_cast<T>(2);
   // TODO : ret < p?
   for (; ret <= p; ret++) {
     bool ok = true;
@@ -93,7 +97,7 @@ int main(void) {
   scanf("%d", &n);
   const auto ps = prime_factorization(n);
   for (const auto [p, e] : ps) {
-    printf("%d^%d = %d ",p, e, fast_pow(p, e));
+    printf("%d^%d = %d ", p, e, fast_pow(p, e));
   }
   printf("\n\n");
 
@@ -103,17 +107,17 @@ int main(void) {
   }
 
   const int n1 = fast_pow(std::get<0>(ps[1]), std::get<1>(ps[1]));
-  assert (n % n1 == 0);
+  assert(n % n1 == 0);
   const int n2 = n / n1;
 
   printf("n1 = %d n2 = %d  n = %d\n", n1, n2, n1 * n2);
 
-  auto [inv_n1,inv_n2] = ext_gcd(n1, n2);
-  inv_n1 = mod(inv_n1, n2);
-  inv_n2 = mod(inv_n2, n1);
+  auto [inv_n1, inv_n2] = ext_gcd(n1, n2);
+  inv_n1                = mod(inv_n1, n2);
+  inv_n2                = mod(inv_n2, n1);
 
-  printf ("n1^(-1)=%d n1xn1^(-1)=%d mod %d\n", inv_n1, mod(n1 * inv_n1, n2), n2);
-  printf ("n2^(-1)=%d n2xn2^(-1)=%d mod %d\n", inv_n2, mod(n2 * inv_n2, n1), n1);
+  printf("n1^(-1)=%d n1xn1^(-1)=%d mod %d\n", inv_n1, mod(n1 * inv_n1, n2), n2);
+  printf("n2^(-1)=%d n2xn2^(-1)=%d mod %d\n", inv_n2, mod(n2 * inv_n2, n1), n1);
 
   assert(mod(n1 * inv_n1, n2) == 1);
   assert(mod(n2 * inv_n2, n1) == 1);
@@ -123,9 +127,9 @@ int main(void) {
   printf("one of a primitive root is %d in mod %d\n", g1, n1);
   const int phi_n1 = euler_phi(n1);
   for (int i = 1; i < phi_n1; i++) {
-    assert(fast_mod_pow(g1, i, n1) != 1 ||  i == n1 - 1);
+    assert(fast_mod_pow(g1, i, n1) != 1 || i == n1 - 1);
   }
-  printf ("%d passed the primitive root test in mod %d\n", g1, n1);
+  printf("%d passed the primitive root test in mod %d\n", g1, n1);
 
   return 0;
 }
--- tests/parts/simple-cooley-tukey01.cc
+++ tests/parts/simple-cooley-tukey01.cc
@@ -1,14 +1,14 @@
-#include <vector>
-#include <complex>
-#include <cstdint>
 #include <algorithm>
 #include <cmath>
+#include <complex>
+#include <cstdint>
+#include <vector>
 
-template<typename T>
+template <typename T>
 static std::vector<std::vector<std::complex<T>>> omega;
-template<typename T>
+template <typename T>
 static std::vector<std::vector<std::complex<T>>> omega_inv;
-template<typename T>
+template <typename T>
 void make_omega_table(const uint_fast8_t logn) {
   size_t i = omega<T>.size();
   if (i >= logn) return;
@@ -17,11 +17,11 @@ void make_omega_table(const uint_fast8_t logn) {
     omega<T>.emplace_back(std::vector<std::complex<T>>());
     omega_inv<T>.emplace_back(std::vector<std::complex<T>>());
     const uint32_t n = (1 << i);
-    const T invn = static_cast<T>(1.0) / n;
+    const T invn     = static_cast<T>(1.0) / n;
     for (size_t j = 0; j < n / 2; j++) {
-      const T tmp = static_cast<T>(-2.0) * M_PI * j * invn; 
-      omega<T>.back().emplace_back(std::cos(tmp),std::sin(tmp));
-      omega_inv<T>.back().emplace_back(std::cos(-tmp),std::sin(-tmp));
+      const T tmp = static_cast<T>(-2.0) * M_PI * j * invn;
+      omega<T>.back().emplace_back(std::cos(tmp), std::sin(tmp));
+      omega_inv<T>.back().emplace_back(std::cos(-tmp), std::sin(-tmp));
     }
   }
 }
@@ -36,105 +36,106 @@ void make_bitrev_table(const uint_fast8_t logn) {
     const uint32_t n = (1 << i);
     for (uint32_t j = 0; j < n; j++) bitrev.back().emplace_back(j);
 
-    std::transform(bitrev.back().begin(), bitrev.back().end(), bitrev.back().begin(), [&i](uint32_t t) {
-        uint32_t ret = 0;
-        for (uint32_t j = 0; j < i; j++){
-          ret <<= 1;
-          ret |= (t & 1);
-          t >>= 1;
-        }
-        return ret;
-      }
-    );
+    std::transform(bitrev.back().begin(), bitrev.back().end(),
+                   bitrev.back().begin(), [&i](uint32_t t) {
+                     uint32_t ret = 0;
+                     for (uint32_t j = 0; j < i; j++) {
+                       ret <<= 1;
+                       ret |= (t & 1);
+                       t >>= 1;
+                     }
+                     return ret;
+                   });
   }
 }
 
-template<typename T>
-static void cooley_tukey_cfft_frequency_inplace(std::complex<T> *a, 
+template <typename T>
+static void cooley_tukey_cfft_frequency_inplace(std::complex<T> *a,
                                                 const uint_fast8_t logn) {
   const uint32_t n = (1 << logn);
   for (uint32_t i = 0; i < logn; i++) {
     const uint32_t offset = (n >> (i + 1));
-    const uint32_t m = (1 << i);
+    const uint32_t m      = (1 << i);
     for (uint32_t j = 0; j < m; j++) {
       for (uint32_t k = 0; k < offset; k++) {
         const uint32_t id = j * 2 * offset + k;
         const auto &tmp_a = a[id];
         const auto &tmp_b = a[id + offset];
-        const auto tmp = tmp_a - tmp_b;
-        a[id] = tmp_a + tmp_b;
-        a[id + offset] = tmp * omega<T>[logn - i][k];
+        const auto tmp    = tmp_a - tmp_b;
+        a[id]             = tmp_a + tmp_b;
+        a[id + offset]    = tmp * omega<T>[logn - i][k];
       }
     }
   }
 }
 
-template<typename T>
-static void cooley_tukey_cfft_time_inplace(std::complex<T> *a, 
-                                                const uint_fast8_t logn) {
+template <typename T>
+static void cooley_tukey_cfft_time_inplace(std::complex<T> *a,
+                                           const uint_fast8_t logn) {
   const uint32_t n = (1 << logn);
   for (uint32_t i = 0; i < logn; i++) {
     const uint32_t offset = (1 << i);
-    const uint32_t m = (n >> (i + 1));
+    const uint32_t m      = (n >> (i + 1));
     for (uint32_t j = 0; j < m; j++) {
       for (uint32_t k = 0; k < offset; k++) {
         const uint32_t id = j * 2 * offset + k;
         a[id + offset] *= omega<T>[i + 1][k];
         const auto &tmp_a = a[id];
         const auto &tmp_b = a[id + offset];
-        const auto tmp = tmp_a - tmp_b;
-        a[id] = tmp_a + tmp_b;
-        a[id + offset] = tmp;
+        const auto tmp    = tmp_a - tmp_b;
+        a[id]             = tmp_a + tmp_b;
+        a[id + offset]    = tmp;
       }
     }
   }
 }
 
-template<typename T>
+template <typename T>
 void cfft_inplace(std::vector<std::complex<T>> *a) {
   if (a->empty()) return;
   const size_t n = a->size();
 
   uint_fast8_t logn = 0;
-  while(!((1 << logn) & n)) logn++;
+  while (!((1 << logn) & n)) logn++;
 
   make_omega_table<T>(logn);
   make_bitrev_table(logn);
 
   for (size_t i = 0; i < n; i++) {
-    if (i < bitrev[logn][i]) swap(a->operator[](i), a->operator[](bitrev[logn][i]));
+    if (i < bitrev[logn][i])
+      swap(a->operator[](i), a->operator[](bitrev[logn][i]));
   }
   cooley_tukey_cfft_time_inplace(a->data(), logn);
 }
 
-template<typename T>
-static void cooley_tukey_cinvfft_frequency_inplace(std::complex<T> *a, 
-                                                const uint_fast8_t logn) {
+template <typename T>
+static void cooley_tukey_cinvfft_frequency_inplace(std::complex<T> *a,
+                                                   const uint_fast8_t logn) {
   const uint32_t n = (1 << logn);
   for (uint32_t i = 0; i < logn; i++) {
     const uint32_t offset = (n >> (i + 1));
-    const uint32_t m = (1 << i);
+    const uint32_t m      = (1 << i);
     for (uint32_t j = 0; j < m; j++) {
       for (uint32_t k = 0; k < offset; k++) {
         const uint32_t id = j * 2 * offset + k;
         const auto &tmp_a = a[id];
         const auto &tmp_b = a[id + offset];
-        const auto tmp = tmp_a - tmp_b;
-        a[id] = tmp_a + tmp_b;
-        a[id + offset] = tmp * omega_inv<T>[logn - i][k];
+        const auto tmp    = tmp_a - tmp_b;
+        a[id]             = tmp_a + tmp_b;
+        a[id + offset]    = tmp * omega_inv<T>[logn - i][k];
       }
     }
   }
 
   for (uint32_t i = 0; i < n; i++) a[i] /= n;
 }
-template<typename T>
+template <typename T>
 void cinvfft_inplace(std::vector<std::complex<T>> *a) {
   if (a->empty()) return;
   const size_t n = a->size();
 
   uint_fast8_t logn = 0;
-  while(!((1 << logn) & n)) logn++;
+  while (!((1 << logn) & n)) logn++;
 
   make_omega_table<T>(logn);
   make_bitrev_table(logn);
@@ -142,51 +143,54 @@ void cinvfft_inplace(std::vector<std::complex<T>> *a) {
   cooley_tukey_cinvfft_frequency_inplace(a->data(), logn);
 
   for (size_t i = 0; i < n; i++) {
-    if (i < bitrev[logn][i]) swap(a->operator[](i), a->operator[](bitrev[logn][i]));
+    if (i < bitrev[logn][i])
+      swap(a->operator[](i), a->operator[](bitrev[logn][i]));
   }
 }
 
 #include <iostream>
 
-int convolution(long long a[],long long b[],long long c[],int n){
-	n *= 2;
-	int N = 2,index = 1;;
-	while (n > N){
-		N *= 2;
-		index++;
-	}
+int convolution(long long a[], long long b[], long long c[], int n) {
+  n *= 2;
+  int N = 2, index = 1;
+  ;
+  while (n > N) {
+    N *= 2;
+    index++;
+  }
   std::vector<std::complex<double>> ac(N);
   std::vector<std::complex<double>> bc(N);
 
   for (int i = 0; i < N; i++) {
-		ac[i] = std::complex<double>((i < n / 2) ? a[i] : 0.0,0.0);
-		bc[i] = std::complex<double>((i < n / 2) ? b[i] : 0.0,0.0);
-	}
+    ac[i] = std::complex<double>((i < n / 2) ? a[i] : 0.0, 0.0);
+    bc[i] = std::complex<double>((i < n / 2) ? b[i] : 0.0, 0.0);
+  }
   cfft_inplace<double>(&ac);
   cfft_inplace<double>(&bc);
-  for (int i = 0; i < N; i++)  {
+  for (int i = 0; i < N; i++) {
     ac.at(i) *= bc.at(i);
   }
   cinvfft_inplace<double>(&ac);
-  for (int i = 0; i < n; i++) 
-		c[i] = ac[i].real() + 0.01;
-	return N;
+  for (int i = 0; i < n; i++) c[i] = ac[i].real() + 0.01;
+  return N;
 }
 
-int convolution2(long long a[],long long b[],long long c[],int n){
-	n *= 2;
-	int N = 2,index = 1;;
-	while (n > N){
-		N *= 2;
-		index++;
-	}
+int convolution2(long long a[], long long b[], long long c[], int n) {
+  n *= 2;
+  int N = 2, index = 1;
+  ;
+  while (n > N) {
+    N *= 2;
+    index++;
+  }
   std::vector<std::complex<double>> ac(N);
 
   for (int i = 0; i < N; i++) {
-		ac[i] = std::complex<double>((i < n / 2) ? a[i] : 0.0,(i < n / 2) ? b[i] : 0.0);
-	}
+    ac[i] = std::complex<double>((i < n / 2) ? a[i] : 0.0,
+                                 (i < n / 2) ? b[i] : 0.0);
+  }
   cfft_inplace<double>(&ac);
-  ac[0] = ac[0].real() * ac[0].imag();
+  ac[0]     = ac[0].real() * ac[0].imag();
   ac[N / 2] = ac[N / 2].real() * ac[N / 2].imag();
   for (int i = 1; i < N / 2; i++) {
     const auto &&tmp = ac[i] * ac[i] - std::conj(ac[N - i] * ac[N - i]);
@@ -197,14 +201,17 @@ int convolution2(long long a[],long long b[],long long c[],int n){
   }
 
   {
-    const auto tmp_r  = (ac[0].real() + ac[N / 2].real()) / 2.0;
-    const auto tmp_i  = (ac[0].real() - ac[N / 2].real()) / 2.0;
+    const auto tmp_r = (ac[0].real() + ac[N / 2].real()) / 2.0;
+    const auto tmp_i = (ac[0].real() - ac[N / 2].real()) / 2.0;
 
     ac[0] = std::complex<double>(tmp_r, tmp_i);
   }
-  for (int i = 1; i < N / 4/*floor*/; i++) {
-    const auto tmp = (std::complex<double>(0.0, 1.0) * std::conj(omega<double>[index][i]) - 1.0) * 0.5;
-    ac[N / 2 - i] = std::conj(ac[N / 2 - i]);
+  for (int i = 1; i < N / 4 /*floor*/; i++) {
+    const auto tmp =
+        (std::complex<double>(0.0, 1.0) * std::conj(omega<double>[index][i]) -
+         1.0) *
+        0.5;
+    ac[N / 2 - i]      = std::conj(ac[N / 2 - i]);
     const auto &&tmp_c = tmp * (ac[i] - ac[N / 2 - i]);
 
     ac[i] += tmp_c;
@@ -218,19 +225,20 @@ int convolution2(long long a[],long long b[],long long c[],int n){
   cinvfft_inplace<double>(&ac);
   for (int i = 0; i < n / 2; i++) {
     c[i * 2 + 0] = ac[i].real() + 0.01;
-		c[i * 2 + 1] = ac[i].imag() + 0.01;
+    c[i * 2 + 1] = ac[i].imag() + 0.01;
   }
-	return N;
+  return N;
 }
 
 long long n;
-long long a[100001],b[100001],c[200002];
+long long a[100001], b[100001], c[200002];
 
 int main() {
-	std::cin >> n;
+  std::cin >> n;
 
-  for (int i = 0; i < n; i++) std::cin >> a[i+1] >> b[i+1];//a[0] = b[0] = 0とする
+  for (int i = 0; i < n; i++)
+    std::cin >> a[i + 1] >> b[i + 1];  // a[0] = b[0] = 0とする
 
-	convolution2(a,b,c,n+1);//0〜nのn+1個
+  convolution2(a, b, c, n + 1);  // 0〜nのn+1個
   for (int i = 0; i < 2 * n; i++) std::cout << c[i + 1] << std::endl;
 }
