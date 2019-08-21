cf9216f663882dde5286b947d1f6279c987623b1
kyawakyawa <ikaairplanef15@yahoo.co.jp>
2019-08-21 17:09:30 +0900

    power4 (only frequency)

--- tests/parts/simple-cooley-tukey02.cc
+++ tests/parts/simple-cooley-tukey02.cc
@@ -61,6 +61,84 @@ void cooley_turkey_fft_power2_frequency_inplace(T* a, const uint32_t n,
 }
 
 template <typename T>
+void cooley_turkey_fft_power4_frequency_inplace(T* a, const uint32_t n,
+                                                char sign = -1) {
+  // a = {Re(a0), Im(a0), Re(a1), Im(a1), ..., Re(an-1), Im(an-1)}
+  if ((n & (n - 1))) return;
+
+  uint32_t m = n;
+  for (; m >= 4; m >>= 2) {
+    const uint32_t m4         = m / 4;
+    const uint32_t num_of_fft = n / m;
+
+    const T theta = static_cast<T>(sign) * 2.0 * M_PI / static_cast<T>(m);
+    for (uint32_t i = 0; i < num_of_fft; i++) {
+      for (uint32_t j = 0; j < m4; j++) {
+        const uint32_t id04 = 2 * (m * i + j);
+        const uint32_t id24 = id04 + 2 * (2 * m4);
+        const uint32_t id14 = id04 + 2 * m4;
+        const uint32_t id34 = id04 + 2 * 3 * m4;
+
+        const T pr = a[id04] + a[id24];
+        const T pi = a[id04 + 1] + a[id24 + 1];
+        const T qr = a[id04] - a[id24];
+        const T qi = a[id04 + 1] - a[id24 + 1];
+        const T rr = a[id14] + a[id34];
+        const T ri = a[id14 + 1] + a[id34 + 1];
+        const T sr = -a[id14 + 1] + a[id34 + 1];
+        const T si = a[id14] - a[id34];
+
+        const T omega1r = std::cos(theta * j);
+        const T omega1i = std::sin(theta * j);
+        const T omega2r = std::fma(static_cast<T>(2.0) * omega1r, omega1r,
+                                   -static_cast<T>(1.0));
+        const T omega2i = static_cast<T>(2.0) * omega1r * omega1i;
+        const T omega3r = std::fma(omega1r, omega2r, -omega1i * omega2i);
+        const T omega3i = std::fma(omega1r, omega2i, omega1i * omega2r);
+
+        a[id04]     = pr + rr;
+        a[id04 + 1] = pi + ri;
+
+        const T tmp_r1 = pr - rr;
+        const T tmp_i1 = pi - ri;
+        a[id14]        = std::fma(tmp_r1, omega2r, -tmp_i1 * omega2i);
+        a[id14 + 1]    = std::fma(tmp_r1, omega2i, tmp_i1 * omega2r);
+
+        const T tmp_r2 = qr - sr;
+        const T tmp_i2 = qi - si;
+        a[id24]        = std::fma(tmp_r2, omega1r, -tmp_i2 * omega1i);
+        a[id24 + 1]    = std::fma(tmp_r2, omega1i, tmp_i2 * omega1r);
+
+        const T tmp_r3 = qr + sr;
+        const T tmp_i3 = qi + si;
+        a[id34]        = std::fma(tmp_r3, omega3r, -tmp_i3 * omega3i);
+        a[id34 + 1]    = std::fma(tmp_r3, omega3i, tmp_i3 * omega3r);
+      }
+    }
+  }
+
+  for (; m > 1; m >>= 1) {
+    const uint32_t m2         = m / 2;
+    const uint32_t num_of_fft = n / m;
+
+    const T theta = static_cast<T>(sign) * 2.0 * M_PI / static_cast<T>(m);
+    for (uint32_t i = 0; i < num_of_fft; i++) {
+      for (uint32_t j = 0; j < m2; j++) {
+        const uint32_t id = m * i + j;
+        const T tmp_r     = a[2 * id] - a[2 * (id + m2)];
+        const T tmp_i     = a[2 * id + 1] - a[2 * (id + m2) + 1];
+        a[2 * id] += a[2 * (id + m2)];
+        a[2 * id + 1] += a[2 * (id + m2) + 1];
+        const T omega_r      = std::cos(theta * j);
+        const T omega_i      = std::sin(theta * j);
+        a[2 * (id + m2)]     = std::fma(tmp_r, omega_r, -tmp_i * omega_i);
+        a[2 * (id + m2) + 1] = std::fma(tmp_r, omega_i, tmp_i * omega_r);
+      }
+    }
+  }
+}
+
+template <typename T>
 void cooley_turkey_fft_power2_time_inplace(T* a, const uint32_t n,
                                            char sign = -1) {
   // a = {Re(a0), Im(a0), Re(a1), Im(a1), ..., Re(an-1), Im(an-1)}
@@ -102,8 +180,8 @@ std::vector<T> convolution(const std::vector<T>& a, const std::vector<T>& b) {
     bc[2 * i]     = (i < b.size()) ? b[i] : static_cast<T>(0.0);
     bc[2 * i + 1] = static_cast<T>(0.0);
   }
-  cooley_turkey_fft_power2_frequency_inplace(ac.data(), N);
-  cooley_turkey_fft_power2_frequency_inplace(bc.data(), N);
+  cooley_turkey_fft_power4_frequency_inplace(ac.data(), N);
+  cooley_turkey_fft_power4_frequency_inplace(bc.data(), N);
   for (size_t i = 0; i < N; i++) {
     const T i2    = 2 * i;
     const T tmp_r = ac[i2];
