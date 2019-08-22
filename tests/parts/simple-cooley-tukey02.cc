bf93f87e0511d748f143136e79751998369aa007
kyawakyawa <ikaairplanef15@yahoo.co.jp>
2019-08-22 12:28:36 +0900

    power4 time

--- tests/parts/simple-cooley-tukey02.cc
+++ tests/parts/simple-cooley-tukey02.cc
@@ -168,6 +168,89 @@ void cooley_turkey_fft_power2_time_inplace(T* a, const uint32_t n,
 }
 
 template <typename T>
+void cooley_turkey_fft_power4_time_inplace(T* a, const uint32_t n,
+                                           char sign = -1) {
+  // a = {Re(a0), Im(a0), Re(a1), Im(a1), ..., Re(an-1), Im(an-1)}
+  if ((n & (n - 1))) return;
+
+  uint32_t index = 0;
+  {
+    uint32_t tmp = n;
+    while (tmp > 1) {
+      index++;
+      tmp *= 2;
+    }
+  }
+
+  uint32_t m = 4;
+  if (index & 1) {
+    const uint32_t num_of_fft = n / 2;
+
+    for (uint32_t i = 0; i < num_of_fft; i++) {
+      const uint32_t id02 = 2 * 2 * i;
+      const uint32_t id12 = id02 + 2 * 1;
+      const T tmp_r       = a[id02];
+      const T tmp_i       = a[id02 + 1];
+      a[id02] += a[id12];
+      a[id02 + 1] += a[id12 + 1];
+      a[id12]     = tmp_r - a[id12];
+      a[id12 + 1] = tmp_i - a[id12 + 1];
+    }
+    m = 8;
+  }
+
+  for (; m <= n; m <<= 2) {
+    const uint32_t m4         = m / 4;
+    const uint32_t num_of_fft = n / m;
+
+    const T theta = static_cast<T>(sign) * 2.0 * M_PI / static_cast<T>(m);
+    for (uint32_t i = 0; i < num_of_fft; i++) {
+      for (uint32_t k = 0; k < m4; k++) {
+        const uint32_t id04 = 2 * (m * i + k);
+        const uint32_t id24 = id04 + 2 * (2 * m4);
+        const uint32_t id14 = id04 + 2 * m4;
+        const uint32_t id34 = id04 + 2 * 3 * m4;
+
+        const T omega1r = std::cos(theta * k);
+        const T omega1i = std::sin(theta * k);
+        const T omega2r = std::fma(static_cast<T>(2.0) * omega1r, omega1r,
+                                   -static_cast<T>(1.0));
+        const T omega2i = static_cast<T>(2.0) * omega1r * omega1i;
+        const T omega3r = std::fma(omega1r, omega2r, -omega1i * omega2i);
+        const T omega3i = std::fma(omega1r, omega2i, omega1i * omega2r);
+
+        const T pr = a[id04];
+        const T pi = a[id04 + 1];
+        const T qr = std::fma(a[id14], omega2r, -a[id14 + 1] * omega2i);
+        const T qi = std::fma(a[id14], omega2i, a[id14 + 1] * omega2r);
+        const T rr = std::fma(a[id24], omega1r, -a[id24 + 1] * omega1i);
+        const T ri = std::fma(a[id24], omega1i, a[id24 + 1] * omega1r);
+        const T sr = std::fma(a[id34], omega3r, -a[id34 + 1] * omega3i);
+        const T si = std::fma(a[id34], omega3i, a[id34 + 1] * omega3r);
+
+        const T xr = pr + qr;
+        const T xi = pi + qi;
+        const T yr = pr - qr;
+        const T yi = pi - qi;
+        const T zr = rr + sr;
+        const T zi = ri + si;
+        const T wr = sign * (-ri + si);
+        const T wi = sign * (rr - sr);
+
+        a[id04]     = xr + zr;
+        a[id04 + 1] = xi + zi;
+        a[id14]     = yr + wr;
+        a[id14 + 1] = yi + wi;
+        a[id24]     = xr - zr;
+        a[id24 + 1] = xi - zi;
+        a[id34]     = yr - wr;
+        a[id34 + 1] = yi - wi;
+      }
+    }
+  }
+}
+
+template <typename T>
 std::vector<T> convolution(const std::vector<T>& a, const std::vector<T>& b) {
   const size_t n = 2 * std::max(a.size(), b.size());
   int N          = 2;
@@ -189,7 +272,7 @@ std::vector<T> convolution(const std::vector<T>& a, const std::vector<T>& b) {
     ac[i2]        = tmp_r * bc[i2] - tmp_i * bc[i2 + 1];
     ac[i2 + 1]    = tmp_r * bc[i2 + 1] + tmp_i * bc[i2];
   }
-  cooley_turkey_fft_power2_time_inplace(ac.data(), N, 1);
+  cooley_turkey_fft_power4_time_inplace(ac.data(), N, 1);
   std::vector<T> ret(n);
   for (size_t i = 0; i < n; i++) {
     ret[i] = (ac[i * 2] / static_cast<T>(N) + 0.01);
