2d8ece5bac1bd5ec160476e19a218475c502029d
kyawakyawa <ikaairplanef15@yahoo.co.jp>
2019-08-22 17:20:50 +0900

    use tcmalloc

--- tests/parts/bootstrap-clang.sh
+++ tests/parts/bootstrap-clang.sh
@@ -2,6 +2,6 @@
 
 rm -rf build
 
-CXX=clang++ CC=clang cmake -Bbuild -H. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 
+CXX=clang++ CC=clang cmake -Bbuild -H. -DWITH_TCMALLOC=On -DCMAKE_EXPORT_COMPILE_COMMANDS=1 
 
 mv build/compile_commands.json .
